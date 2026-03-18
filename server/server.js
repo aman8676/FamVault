b22ef30add76a715280169051f77e59c6d231a4f
Aman_8676 <jalanaman2002@gmail.com>
2026-03-18 23:34:54 +0530

    resolved cors issue

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // ← add this (your frontend URL)
+    origin: "https://fam-vault.vercel.app/",
     credentials: true,
   }),
 );
