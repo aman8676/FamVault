5a92896892e1596fadae1156854d9b09844f7b6b
Aman_8676 <jalanaman2002@gmail.com>
2026-03-19 00:19:13 +0530

    changed the multer issue for empty folder render

--- server/middlewares/multer.middleware.js
+++ server/middlewares/multer.middleware.js
@@ -1,15 +1,19 @@
-import multer from 'multer';
+import multer from "multer";
+import fs from "fs";
+
+// Creates the folder if it doesn't exist (fixes Render deployment issue)
+fs.mkdirSync("./public/temp", { recursive: true });
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
-    const uniqueName = `${Date.now()}-${file.originalname}`
-    cb(null,uniqueName);
+    const uniqueName = `${Date.now()}-${file.originalname}`;
+    cb(null, uniqueName);
   },
 });
 
 export const upload = multer({
-    storage
-})
+  storage,
+});
